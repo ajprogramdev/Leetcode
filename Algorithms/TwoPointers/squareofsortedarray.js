f261ef686afce9f316c7e9da75c0af1d488240fb
AJ <ajprogramdevelopment@gmail.com>
2022-06-22 15:03:16 -0400

    Easy Two Pointer squareofsortedarray

---
+++ Algorithms/TwoPointers/squareofsortedarray.js
@@ -0,0 +1,32 @@
+/*
+Given an integer array nums sorted in non-decreasing order,
+return an array of the squares of each number sorted in non-decreasing order.
+
+Example 1:
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+
+Example 2:
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+*/
+
+/*
+Steps:
+1.Loop through array, calculate the square of each value.
+2.Sort the array.
+3.return the sorted array.
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var sortedSquares = function(nums) {
+    for (let i=0; i < nums.length; i++) {        
+        nums[i] = nums[i] * nums[i];
+    }        
+    return nums.sort((a,b) => a-b);    
+};
