ee3557a74a38c4b5f179f929b578c1a2fb3d4c0b
ajprogramdev <ajprogramdevelopment@gmail.com>
2022-09-27 17:57:50 -0400

    Easy #121 Besttimetosellstock

---
+++ DS/AlgoReference/LinkedList.java
@@ -0,0 +1,11 @@
+package DS.AlgoReference;
+
+public class LinkedList {
+
+    public void exceptions() {
+        /*
+         * Error Handling
+         */
+        throw new NullPointerException("some text describing here");
+    }
+}
