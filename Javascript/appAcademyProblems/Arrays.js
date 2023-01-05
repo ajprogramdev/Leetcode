ab33b96e99c80414528c37f9e97597cd52da7fbb
ajprogramdev <ajprogramdevelopment@gmail.com>
2023-01-05 17:25:10 -0400

    [logBetweenStepper]

--- Javascript/appAcademyProblems/Arrays.js
+++ Javascript/appAcademyProblems/Arrays.js
@@ -32,5 +32,45 @@ function logBetween(lowNum, highNum){
     return numsArr;
   }
   
-
 console.log(logBetween(-1, 2));
+
+/*
+logBetweenStepper
+Write a function logBetweenStepper(min, max, step) that takes in three numbers as parameters. 
+The function should return an array of numbers between min and max at step intervals.
+
+Examples:
+logBetweenStepper(5, 9, 1) // => [5, 6, 7, 8, 9]
+logBetweenStepper(-10, 15, 5) // => [-10, -5, 0, 5, 10, 15]
+
+Approach:
+1.Create numbers array.
+2.Push initial min into array.
+2.Edge Cases
+3.Loop size between min and max.
+         i = i + step;
+         i < max, push i to array.
+         i > max, push max to array, return array.
+4.Return array.        
+*/
+function logBetweenStepper(min, max, step) {
+
+    const numArr = [];
+    numArr.push(min);
+   
+    let i = min;
+     while (i < max){
+      i = i + step;
+       if (i > max){
+             numArr.push(max);
+             return numArr;
+         }  
+        numArr.push(i);
+       
+     }
+     
+     return numArr;  
+ }
+
+console.log(logBetweenStepper(5, 9, 1)); // => [5, 6, 7, 8, 9]
+console.log(logBetweenStepper(-10, 15, 5)); // => [-10, -5, 0, 5, 10, 15])
