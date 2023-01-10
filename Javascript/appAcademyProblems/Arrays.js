c823438ebd1b15f0e6e194889feae086d041cc65
ajprogramdev <ajprogramdevelopment@gmail.com>
2023-01-09 22:41:52 -0400

    peakfinder

--- Javascript/appAcademyProblems/Arrays.js
+++ Javascript/appAcademyProblems/Arrays.js
@@ -597,3 +597,53 @@ function isPrime(number) {
 console.log(firstNPrimes(0));  // => []
 console.log(firstNPrimes(1));  // => [2]
 console.log(firstNPrimes(4));  // => [2, 3, 5, 7]
+
+/*
+Write a function peakFinder(array) that takes in an array of numbers. 
+It should return an array containing the indices of all the peaks. 
+A peak is an element that is greater than both of its neighbors.
+ If it is the first or last element, it is a peak if it is greater than its one neighbor. Assume the array has a length of at least 2.
+
+Examples:
+peakFinder([1, 2, 3, 2, 1]); // => [2]
+peakFinder([2, 1, 2, 3, 4, 5]); // => [0, 5]
+peakFinder([4, 6, 9, 4, 2, -7, 2, -4, 5]); // => [2, 6, 8]
+
+Approach:
+1.Edge Cases
+2.Create an array to store the peaks.
+3.Loop through the array 
+    compare the index value with its left and right index values.
+    if greater than left and right, push to array 
+        if no value on one side, indexValue is larger than the other side, push to array.
+
+4. return array peaks
+*/
+
+function peakFinder(array) {
+    const peaks = [];
+
+    if (array[0] > array[1]) {
+        peaks.push(0);
+    }
+    
+    for (let i=1; i < array.length-1; i++) {       
+        if (array[i] > array[i-1] && array[i] > array[i+1] ) {
+            peaks.push(i);
+        }       
+    }
+  
+    if (array[array.length-1] > array[array.length-2]) {
+        peaks.push(array.length-1);
+    }
+
+    return peaks;
+}
+
+console.log(peakFinder([1, 2, 3, 2, 1])); // => [2]
+console.log(peakFinder([2, 1, 2, 3, 4, 5])); // => [0, 5]
+console.log(peakFinder([4, 6, 9, 4, 2, -7, 2, -4, 5])); // => [2, 6, 8]
+
+console.log(peakFinder([1, 2, 3, 2, 1])); // => [2]
+console.log(peakFinder([2, 1, 2, 3, 4, 5])); // => [0, 5]
+console.log(peakFinder([4, 6, 9, 4, 2, -7, 2, -4, 5])); // => [2, 6, 8]
